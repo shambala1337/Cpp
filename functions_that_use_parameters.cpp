74868d11d41ef8fc11bef2e3815ea1522e50cb7c
Shambala <30989643+shambala1337@users.noreply.github.com>
2019-04-08 23:11:24 -0500

    Add files via upload

---
+++ functions_that_use_parameters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printCrap(int x) {
+	cout << "my favorite number " << x <<" " <<  y << endl;
+}
+
+int main()
+{
+	printCrap(50);
+
+	system("pause");
+}
